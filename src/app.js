6f9f95eb5c331334cb35b560c1f01a98ace18563
Khris Hernandez <contact@devkhris.com>
2022-04-20 17:54:27 -0700

    Added PostController and updated routes

--- src/app.js
+++ src/app.js
@@ -2,21 +2,9 @@ const { req, res } = require('express')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const postsController = require('./controllers/posts.controller')
 const port = 3000
 const app = express()
-
-let posts = [
-    {
-        id: 1,
-        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        body: "Voluptates quisquam tempora placeat quia ullam recusandae commodi possimus ad nulla sit animi quod deleniti, quam reprehenderit nesciunt, accusantium modi quis! Ab, corrupti velit est eaque incidunt tempora assumenda inventore facere dignissimos?"
-    },
-    {
-        id: 2,
-        title: "Voluptates quisquam tempora placeat quia ullam recusandae",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit commodi possimus ad nulla sit animi quod deleniti, quam reprehenderit nesciunt, accusantium modi quis! Ab, corrupti velit est eaque incidunt tempora assumenda inventore facere dignissimos?"
-    },
-]
 
 
 const start = () => {
@@ -30,65 +18,34 @@ const start = () => {
     })
 
     app.get('/posts', (req, res) => {
-        res.status(200)
-        res.send(posts)
+        let { code, content } = postsController.getPosts()
+        res.status(code)
+        res.send(content)
         res.end()
     })
 
     app.get('/posts/:id', (req, res) => {
-        let id = parseInt(req.params.id)
-
-        let post = posts.find(post => post.id === id)
-
-        res.status(200)
-        res.send(post)
+        let { code, content } = postsController.findPostById(req.params.id)
+        res.status(code)
+        res.send(content)
     })
 
     app.post('/posts', (req, res) => {
-
-        let post = { id, title, body } = req.body
-
-        post.id = Math.floor(Math.random() * 1000 / 7 );
-
-        posts.push(post)
-
-        res.status(201)
-        res.send(posts.filter(result => result.id === post.id))
+        let { code, content } = postsController.createPost(req.body)
+        res.status(code)
+        res.send(content)
     })
 
     app.patch('/posts/:id', (req, res) => {
-        let id = parseInt(req.params.id)
-
-        let post = posts.find(post => post.id === id)
-
-        const index = posts.indexOf(post)
-
-        post = { title, body } = req.body
-        post.id = id
-
-        if(index > -1 ) {
-            posts.splice(index, 1)
-        }
-
-        posts[index] = post
-
-        res.status(200)
-        res.send('Post updated succesfully')
+        let { code, content } = postsController.updatePost(req.params.id, req.body)
+        res.status(code)
+        res.send(content)
     })
 
     app.delete('/posts/:id', (req, res) => {
-        let id = parseInt(req.params.id)
-
-        let post = posts.find(post => post.id === id)
-
-        const index = posts.indexOf(post)
-        
-        if(index > -1) {
-            posts.splice(index, 1)
-
-        }
-        res.status(200)
-        res.send('Post deleted succesfully')
+        let { code, content } = postsController.deletePost(req.params.id)
+        res.status(code)
+        res.send(content)
     })
     
     app.listen(port, () => {
